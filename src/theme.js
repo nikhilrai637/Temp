111672d92082b6868640cdd12be389fb2d6e49d2
Anjali_Srivastava <43489841+AnjaliSrivastava722@users.noreply.github.com>
2020-09-08 23:58:26 +0530

    Update

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary:{
+      main: "#1098F7",
+    },
+     secondary: {
+      main: "#fbb13c",
+    },
+  },
+  typography: {
+
+    fontFamily:'Quicksand',
+
+     
+
+  },
+});
+ export default theme
